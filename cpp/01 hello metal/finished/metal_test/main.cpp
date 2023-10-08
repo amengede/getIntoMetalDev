468e3f082bf685b9c822828fd5be1f37d8ca1264
Andrew Mengede <secret@fake_server.com>
2023-10-08 13:12:47 +1000

    C++ setup

---
+++ cpp/01 hello metal/finished/metal_test/main.cpp
@@ -0,0 +1,22 @@
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+
+#include "config.h"
+#include "control/app_delegate.h"
+
+int main( int argc, char* argv[] )
+{
+    NS::AutoreleasePool* autoreleasePool = NS::AutoreleasePool::alloc()->init();
+
+    AppDelegate controller;
+
+    NS::Application* app = NS::Application::sharedApplication();
+    app->setDelegate( &controller );
+    app->run();
+
+    autoreleasePool->release();
+
+    return 0;
+}
---
+++ cpp/01 hello metal/start/hello_metal_cpp/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  hello_metal_cpp
+//
+//  Created by Andrew Mengede on 15/9/2023.
+//
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+
+#include <Metal/Metal.hpp>
+#include <Appkit/AppKit.hpp>
+#include <Metalkit/MetalKit.hpp>
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
