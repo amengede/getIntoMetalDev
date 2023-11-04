0893d0fe799903c93496929ed4797561efac74e8
Dr. Colin Hirsch <github@colin-hirsch.net>
2023-11-04 16:37:39 +0100

    Move private implementations to separate file.

--- cpp/01 hello metal/finished/hello_metal_cpp/main.cpp
+++ cpp/01 hello metal/finished/hello_metal_cpp/main.cpp
@@ -5,11 +5,6 @@
 //  Created by Andrew Mengede on 15/9/2023.
 //
 
-#define NS_PRIVATE_IMPLEMENTATION
-#define MTL_PRIVATE_IMPLEMENTATION
-#define MTK_PRIVATE_IMPLEMENTATION
-#define CA_PRIVATE_IMPLEMENTATION
-
 #include "config.h"
 #include "control/app_delegate.h"
 
---
+++ cpp/01 hello metal/finished/hello_metal_cpp/private.cpp
@@ -0,0 +1,13 @@
+//
+//  private.cpp
+//  hello_metal_cpp
+//
+//  Created by Dr. Colin Hirsch on 04/11/23.
+//
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+
+#include "config.h"
